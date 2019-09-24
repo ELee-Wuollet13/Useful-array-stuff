21a12ca16350f2b54c885a590c5533a83a60a3c0
Angela Weber and Everett Lee-Wuollet <example@example.com>
2019-09-23 20:48:41 -0700

    more array stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
-  $("form").submit(function(event) {
+  $("#blanks form").submit(function(event) {
 
     event.preventDefault();
-    var favs = []
+    var favs = ["fav1", "fav2", "fav3", "fav4", "fav5", "fav6"];
+    var favResult = []
+    favs.forEach(function(fav) {
+      var userInput = $("input#" + fav).val();
+      $("." + fav).text(favResult);
+      favResult.push(userInput);
+    });
+    // console.log(favResult);
 
-    favs.push(($('input#fav1').val()), ($('input#fav2').val()), ($('input#fav3').val()) , ($('input#fav4').val()), ($('input#fav5').val()), ($('input#fav6').val()));
-    
+
+
+
+    // favs.push(($('input#fav1').val()), ($('input#fav2').val()), ($('input#fav3').val()) , ($('input#fav4').val()), ($('input#fav5').val()), ($('input#fav6').val()));
     // var fav1Input = $('input#fav1').val();
     // var fav2Input = $('input#fav2').val();
     // var fav3Input = $('input#fav3').val();
@@ -15,7 +24,6 @@ $(document).ready(function() {
     //
     // favs.push(fav1Input, fav2Input, fav3Input, fav4Input, fav5Input, fav6Input);
     // $('#fav1').text(fav1Input);
-    console.log(favs);
     // alert('is this thing working?')
   });
 });
